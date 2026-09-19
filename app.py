fdf0a8b3711a5b1fc87912d4516c50b7d6a35c5f
denisvillalbarojas <denisvillalbarojas@gmail.com>
2026-09-19 16:27:35 -0500

    texto 04

--- app.py
+++ app.py
@@ -2913,13 +2913,20 @@ st.markdown(
     .st-key-cantidad_procedimiento div[data-baseweb="input"],
     .st-key-cantidad_procedimiento div[data-baseweb="input"] > div,
     .st-key-cantidad_procedimiento input {
-        background-color: #FFF3C4 !important;
+        background-color: #FFF7D7 !important;
     }
 
     /* Cajas del grupo "Biopsias: Cantidades" */
     [data-testid="stMain"] [class*="st-key-cant_biopsia_"] div[data-baseweb="input"],
     [data-testid="stMain"] [class*="st-key-cant_biopsia_"] div[data-baseweb="input"] > div,
     [data-testid="stMain"] [class*="st-key-cant_biopsia_"] input {
+        background-color: #FFF7D7 !important;
+    }
+
+    /* Cajas del grupo "Procedimientos adicionales: Cantidad" */
+    [data-testid="stMain"] [class*="st-key-cant_adicional_"] div[data-baseweb="input"],
+    [data-testid="stMain"] [class*="st-key-cant_adicional_"] div[data-baseweb="input"] > div,
+    [data-testid="stMain"] [class*="st-key-cant_adicional_"] input {
         background-color: #FFF3C4 !important;
     }
 
