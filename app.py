d71c0103ff04bfda6e3869d300c7e9a544895fd7
denisvillalbarojas <denisvillalbarojas@gmail.com>
2026-09-19 15:32:29 -0500

    comentarios a cajas de texto

--- app.py
+++ app.py
@@ -3250,7 +3250,7 @@ if opcion_menu == "🏠 Formulario":
                 with col_otro_proc:
                     valor_otro_procedimiento = st.text_input(
                         "Si seleccionaste Otro",
-                        placeholder="PUEDE IR VACIO",
+                        placeholder="PUEDE IR VACIO / Nombre Otro",
                         help='Completar solo si eligió "Otros" arriba.',
                         key="otro_procedimiento",
                     )
@@ -3258,6 +3258,7 @@ if opcion_menu == "🏠 Formulario":
                 with col_cantidad:
                     valor_cantidad = st.text_input(
                         "Cantidad (Procedimiento) *",
+                        placeholder="PUEDE IR VACIO / Nombre Otro",
                         help="Cantidad del procedimiento seleccionado.",
                     )
 
